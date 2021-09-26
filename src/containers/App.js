5749dd1249541efbf995831222260ce110816905
Tomas Macchi <muma.sanmartin2011@gmail.com>
2021-09-26 11:46:57 -0300

    s

--- src/containers/App.js
+++ src/containers/App.js
@@ -31,7 +31,7 @@ function App() {
 
   function onSearch(ciudad) {
     //Llamado a la API del clima
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}`)
       .then(r => r.json())
       .then((recurso) => {
         if(recurso.main !== undefined){
